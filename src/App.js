41e7dd88872a15536b950377da0ef519febe0f4b
yashvanthkg <yashvanth1590@gmail.com>
2020-11-11 10:08:32 +0530

    use arrry with useState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
-import ClassCounter from './components/ClassCounter';
-import HookCounter from './components/HookCounter';
-import HookCounter2 from './components/HookCounter2';
-import HookCounter3 from './components/HookCounter3';
+import HookCounter4 from './components/HookCounter4';
 
 
 function App() {
   return (
-    <div className="App">
-      {/* <ClassCounter /> */}
-      {/* <HookCounter /> */}
-      {/* <HookCounter2 /> */}
-      <HookCounter3 />
+    <div>
+      <HookCounter4 />
     </div>
   );
 }
