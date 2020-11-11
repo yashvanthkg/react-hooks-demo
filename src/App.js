120efd963b1a6b56d85d4a76b1ccdaad37e28dfd
yashvanthkg <yashvanth1590@gmail.com>
2020-11-11 07:49:55 +0530

    functional component and add hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
 
 
 function App() {
   return (
     <div className="App">
       <ClassCounter />
+      <HookCounter />
     </div>
   );
 }
