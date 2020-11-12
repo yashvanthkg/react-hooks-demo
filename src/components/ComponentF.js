7519908e7c09fc1a499deab73cc2c0477c8f235d
yashvanthkg <yashvanth1590@gmail.com>
2020-11-12 13:33:35 +0530

    useContext hook

--- src/components/ComponentF.js
+++ src/components/ComponentF.js
@@ -1,16 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { UserContext } from '../App'
 
 function ComponentF() {
+    const user = useContext(UserContext)
     return (
         <div>
-            <UserContext.Consumer>
-                {
-                    user => {
-                        return <div>User context value: {user}</div>
-                    }
-                }
-            </UserContext.Consumer>
+             User context value: {user}
         </div>
     )
 }
