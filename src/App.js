64e3ae18fefc3dd55d518a03d0909d21fdb18402
yashvanthkg <yashvanth1590@gmail.com>
2020-11-12 19:32:31 +0530

    useReducer with useContext

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useReducer,createContext } from 'react'
 import './App.css';
-import CounterOne from './components/CounterOne';
-import CounterTwo from './components/CounterTwo';
-import CounterThree from './components/CounterThree';
+import ComponentA from './components/ComponentA';
+import ComponentB from './components/ComponentB';
+import ComponentC from './components/ComponentC';
 
+export const CountContext = createContext()
+const initialState = 0
+const reducer = (state, action) => {
+    switch(action) {
+        case 'increment':
+        return state + 1
+        case 'decrement':
+            return state - 1
+        case 'reset':
+            return initialState
+        default: return state
+    }
+}
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
-    <div className='App'>
-      {/* <CounterOne /> */}
-      {/* <CounterTwo /> */}
-      <CounterThree />
-    </div>
+    <CountContext.Provider value={{countState: count, countDispatch: dispatch}}>
+        <div className='App'>
+          Count - {count}
+          <ComponentA />
+          <ComponentB />
+          <ComponentC />
+        </div>
+    </CountContext.Provider>
   );
 }
 
--- src/components/ComponentD.js
+++ src/components/ComponentD.js
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react'
 import { CountContext } from '../App'
 
-function ComponentF() {
+function ComponentD() {
     const countContext = useContext(CountContext)
 
     return (
         <div>
-            Component F-{countContext.countState}
+            Component D-{countContext.countState}
             <button onClick={() => countContext.countDispatch('increment')}>Increment</button>
             <button onClick={() => countContext.countDispatch('decrement')}>Decrement</button>
             <button onClick={() => countContext.countDispatch('reset')}>Reset</button>
@@ -14,4 +14,4 @@ function ComponentF() {
     )
 }
 
-export default ComponentF
+export default ComponentD
