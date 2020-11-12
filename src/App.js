ac0c0672dba4a693b45fd4efce4d63e9bda6a8de
yashvanthkg <yashvanth1590@gmail.com>
2020-11-12 17:01:32 +0530

    useReducer hook with simple state and action

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react'
 import './App.css';
-import ComponentC from './components/ComponentC';
+import CounterOne from './components/CounterOne';
 
-export const UserContext = React.createContext()
-export const ChannelContext  = React.createContext()
 function App() {
   return (
     <div className='App'>
-      <UserContext.Provider value={'Yashvanth'}>
-        <ChannelContext.Provider value = {"codevolution"}>
-          <ComponentC />
-        </ChannelContext.Provider>
-      </UserContext.Provider>
+      <CounterOne />
     </div>
   );
 }
