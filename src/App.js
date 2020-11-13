76f4e6a474f0250032dfc2a5a25fd5b9993307be
yashvanthkg <yashvanth1590@gmail.com>
2020-11-13 09:23:08 +0530

    fecth data using useEffct and useReducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
 import DataFetchingOne from './components/DataFetchingOne';
+import DataFetchingTwo from './DataFetchingTwo';
 
 function App() {
   return(
     <div className="App">
-      <DataFetchingOne />
+      {/* <DataFetchingOne /> */}
+      <DataFetchingTwo />
     </div>
   )
 
