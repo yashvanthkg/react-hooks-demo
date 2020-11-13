418aea4b76569712987c2c143bc99ca3f5626878
yashvanthkg <yashvanth1590@gmail.com>
2020-11-13 14:24:24 +0530

    useRef hook

---
+++ useref-hook/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import FocusInput from './components/FoucsInput'
+
+function App() {
+
+    return (
+    <div className="App">
+      <FocusInput />
+    </div>
+  );
+}
+
+export default App;
